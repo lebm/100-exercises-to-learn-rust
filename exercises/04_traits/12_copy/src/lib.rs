1e4e3501ad8cdd68a9a8a509a481ea45c5dc1d0c
Luis Bravo <ledbrv@gmail.com>
2024-11-15 09:52:53 -0300

    Parei em 04/13

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,11 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+#[allow(unused_imports)]
+use std::env::consts::OS;
+use std::ops::Add;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +13,16 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+impl Add for WrappingU32 {
+    type Output = WrappingU32;
+
+    fn add(self, other: WrappingU32) -> WrappingU32 {
+        WrappingU32 {
+            value: self.value.wrapping_add(other.value),
+        }
     }
 }
 
